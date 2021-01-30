14b95b6c35c14499ee347a6a7e7c6eec7f0de6d3
Astlodie <71849560+Astlodie@users.noreply.github.com>
2021-01-30 13:44:08 -0500

    Added the break statement.

--- src/PrintDayInWord.java
+++ src/PrintDayInWord.java
@@ -1,22 +1,29 @@
 
 public class PrintDayInWord {
 public static void main(String[] args) {
-	int dayNumber=0;
+	int dayNumber=5;
 	switch (dayNumber) {
 	case 1:
 	System.out.println("Sunday");
+	break;
 	case 2:
 	System.out.println("Monday");
+	break;
 	case 3:
 	System.out.println("Tuesday");
+	break;
 	case 4:
 	System.out.println("Wednesday");
+	break;
 	case 5:
 	System.out.println("Thursday");
+	break;
 	case 6:
 	System.out.println("Friday");
+	break;
 	case 7:
 	System.out.println("Saturday");
+	break;
 	default:
 	System.out.println("Not a valid day");
 				}
