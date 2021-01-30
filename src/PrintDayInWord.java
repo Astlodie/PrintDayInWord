a145f03f6e224b0901750fd81817a8233ecb7e4c
Astlodie <71849560+Astlodie@users.noreply.github.com>
2021-01-30 13:27:55 -0500

    Program with switch-case-default statement.

---
+++ src/PrintDayInWord.java
@@ -0,0 +1,25 @@
+
+public class PrintDayInWord {
+public static void main(String[] args) {
+	int dayNumber=0;
+		switch (dayNumber) {
+	case 1:
+	System.out.println("Sunday");
+	case 2:
+		System.out.println("Monday");
+	case 3:
+		System.out.println("Tuesday");
+	case 4:
+		System.out.println("Wednesday");
+	case 5:
+		System.out.println("Thursday");
+	case 6:
+		System.out.println("Friday");
+	case 7:
+		System.out.println("Saturday");
+	default:
+		System.out.println("Not a valid day");
+				}
+	}
+
+}
